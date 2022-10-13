d29dd593a5ddb69fa46076c86f8691f6165fcecb
Andrey Sozykin <sozykin@gmail.com>
2022-10-13 20:30:01 +0500

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -38,7 +38,7 @@ def print_predictions(preds):
 
 model = load_model()
 
-st.title('Новая улучшенная классификации изображений')
+st.title('Классификации изображений в облаке Streamlit')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
